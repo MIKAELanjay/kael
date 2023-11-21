d9bb196705a740c1a2b3686f2c426874dc1427d2
MIKAELanjay <150810222+MIKAELanjay@users.noreply.github.com>
2023-11-21 20:17:58 +0700

    Add files via upload

---
+++ import random.py
@@ -0,0 +1,10 @@
+import random 
+
+sandi = "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+panjang = int(input("berapa panjang karakter untuk kata sandi:"))
+hasil = ""
+
+for i in range(panjang):
+    hasil += random.choice(sandi)
+print("kata sandi yang dihasilkan:", hasil)
+
